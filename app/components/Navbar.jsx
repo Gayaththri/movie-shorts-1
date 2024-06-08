b9d81fc4cbffada3dbe07683f29fc09167d832ae
Gayaththri <sriigaya3@gmail.com>
2024-06-08 10:55:25 +0530

    Refactor navbar component to use Kinde authentication and link components.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
+
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
       {/* Div for the left side */}
@@ -13,8 +21,20 @@ export default function Navbar() {
 
       {/* Div for the right side */}
       <div className="flex items-center gap-x-5 pr-5">
-        <Button>Log In</Button>
-        <Button variant="secondary">Sign Up</Button>
+        {(await isAuthenticated()) ? (
+          <LogoutLink>
+            <Button>Log out</Button>
+          </LogoutLink>
+        ) : (
+          <div className="flex items-center gap-x-5 pr-5">
+            <LoginLink>
+              <Button>Log In</Button>
+            </LoginLink>
+            <RegisterLink>
+              <Button variant="secondary">Sign Up</Button>
+            </RegisterLink>
+          </div>
+        )}
       </div>
     </nav>
   );
